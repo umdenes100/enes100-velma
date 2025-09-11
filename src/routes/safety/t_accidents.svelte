20b1d105774ee612aa210213e2c086bcae559e86
M-Crooks <mcrooks5@terpmail.umd.edu>
2025-09-11 19:04:55 -0400

    Created files for eventual safety tracker tabs and page. Started thinking about layout and organization. Only empty files and commented code currently.

---
+++ src/routes/safety/t_accidents.svelte
@@ -0,0 +1,18 @@
+<!-- <script lang="ts">
+    import tabName from "tabFileName";
+
+    let tabs = [
+        { name: "tab name", comp: tabName },
+    ];
+
+    let cur = tabs[0];
+</script>
+
+<div class="text">
+    {#each tabs as tab}
+        <button class:selected={cur === tab} on:click={() +> (cur = tab)}>
+            {tab.name}
+        </button>
+    {/each}
+    <svelte:component this={cur.comp} />
+</div> -->
