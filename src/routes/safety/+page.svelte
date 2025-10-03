1d83f97c288187273d0e3c3320166a2cc2168eba
cmwharff <115747231+cmwharff@users.noreply.github.com>
2025-10-03 13:21:34 -0400

    made iframe transparent

--- src/routes/safety/+page.svelte
+++ src/routes/safety/+page.svelte
@@ -1,3 +1,3 @@
 <div class="text">
-    <iframe src="https://umdenes100.github.io/enes100-safetylog/" title="safety" height="100%" width="100%" style="background-color: #121B21;"></iframe>
+    <iframe src="https://umdenes100.github.io/enes100-safetylog/" title="safety" height="100%" width="100%" style="background: none transparent; border:none;"></iframe>
 </div>
