666de464ed7552d88f33544054ecc0465fe64a02
M-Crooks <mcrooks5@terpmail.umd.edu>
2026-02-23 12:52:56 -0500

    added a student-facing toolcheck page that doesn't have a menu to the rest of velma

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,14 +8,18 @@
     <title>VELMA</title>
 </svelte:head>
 
-{#if $page.url.pathname != "/enes100-velma/website/"}
+{#if $page.url.pathname == "/enes100-velma/public-tools/"}
     <div class="page">
-        <Index></Index>
+        <slot></slot>
+    </div>
+{:else if $page.url.pathname == "/enes100-velma/website/"}
+    <div class="page">
+        <IndexWeb></IndexWeb>
         <slot></slot>
     </div>
 {:else}
     <div class="page">
-        <IndexWeb></IndexWeb>
+        <Index></Index>
         <slot></slot>
     </div>
 {/if}
---
+++ src/routes/public-tools/+page.svelte
@@ -0,0 +1,3 @@
+<div class="text">
+    <iframe src="https://umdenes100.github.io/enes100-toolcheck/" title="public-tools" height="100%" width="90%" style="background-color: #121B21;"></iframe>
+</div>
