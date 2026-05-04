b78667704f4e5988e68b4c9db0aa2e061468572e
M-Crooks <mcrooks5@terpmail.umd.edu>
2026-05-04 13:46:23 -0400

    fixed images for seed

--- src/routes/milestone/+page.svelte
+++ src/routes/milestone/+page.svelte
@@ -40,16 +40,16 @@
                             if (seed && seed instanceof HTMLDivElement) {
                                 // seed.innerHTML = `<div>Plantable plot: ${plots[plot]}</div><img src="/enes100-velma/img/mission_diagram_seed.png" alt="seed mission diagram" width="125">`;
                                 if (plot == 4) {
-                                    seed.innerHTML = `<div>Plot setup: both plantable</div><img src="/enes100-velma/static/img/seed_plot_1.png" alt="seed mission diagram" width="125">`;
+                                    seed.innerHTML = `<div>Plot setup: both plantable</div><img src="/enes100-velma/static/img/seed_plot_4.png" alt="seed mission diagram" width="125">`;
                                 }
                                 if (plot == 3) {
-                                    seed.innerHTML = `<div>Plot setup: neither plantable</div><img src="/enes100-velma/img/seed_plot_2.png" alt="seed mission diagram" width="125">`;
+                                    seed.innerHTML = `<div>Plot setup: neither plantable</div><img src="/enes100-velma/img/seed_plot_3.png" alt="seed mission diagram" width="125">`;
                                 }
                                 if (plot == 2) {
-                                    seed.innerHTML = `<div>Plot setup: plantable-adjacent</div><img src="/enes100-velma/img/seed_plot_3.png" alt="seed mission diagram" width="125">`;
+                                    seed.innerHTML = `<div>Plot setup: plantable-adjacent</div><img src="/enes100-velma/img/seed_plot_2.png" alt="seed mission diagram" width="125">`;
                                 }
                                 if (plot == 1) {
-                                    seed.innerHTML = `<div>Plot setup: plantable-diagonal</div><img src="/enes100-velma/img/seed_plot_4.png" alt="seed mission diagram" width="125">`;
+                                    seed.innerHTML = `<div>Plot setup: plantable-diagonal</div><img src="/enes100-velma/img/seed_plot_1.png" alt="seed mission diagram" width="125">`;
                                 }
                             }
                             break;
